fcf45af249eb34d51e0aa9a85d053107402909f1
Jessica Glover <glover.jessica.m@gmail.com>
2022-08-25 19:28:22 -0400

    Added logic for inserting token into the right blank space in a column, added stub to check win condition, and added initial gameplay loop

--- game.py
+++ game.py
@@ -25,6 +25,8 @@ class Slot:
 
 class Board:
 
+    num_cols = 7
+    num_rows = 6
     index_to_col = {0: "A", 1: "B", 2: "C", 3: "D", 4: "E", 5: "F", 6: "G"}
     col_to_index = {val: key for key, val in index_to_col.items()}
 
@@ -41,33 +43,53 @@ class Board:
         return self_string
 
     def fill_new_board(self):
-        for col in range(7):
+        for col in range(Board.num_cols):
             self.slots.append([])
-            for row in range(6):
+            for row in range(Board.num_rows):
                 self.slots[col].append(Slot(Color.BLANK, (Board.index_to_col[col], row)))
 
 
 class Player:
 
-    def __init__(self, name, color, my_turn):
+    def __init__(self, name, color):
         self.name = name
         self.color = color
-        self.my_turn = my_turn
         self.num_tokens = 21
 
 
 class Game:
 
-    def __init__(self, player1=Player("Player 1", Color.RED, True), player2=Player("Player 2", Color.YELLOW, False)):
+    def __init__(self, player1=Player("Player 1", Color.RED), player2=Player("Player 2", Color.YELLOW)):
         self.player1 = player1
         self.player2 = player2
         self.board = Board()
         self.players = [self.player1, self.player2]
 
-    def insert_token(self, player, column=0):
-        self.board.slots[column][0] = Slot(player.color, (column, 0))
+    def insert_token(self, player, column_string):
+        col = Board.col_to_index[column_string]
+        first_blank_slot_row = 0
+        for i in range(Board.num_rows):
+            if self.board.slots[col][i].state == Color.BLANK:
+                first_blank_slot_row = i
+                break
+            if i == Board.num_rows - 1:
+                print("Invalid play - column {} is full.".format(column_string))
+                return False
+        self.board.slots[col][first_blank_slot_row] = Slot(player.color, (col, first_blank_slot_row))
+        return True
+
+    def play_turn(self, player):
+        finished = False
+        print("It's {}'s turn.".format(player.name))
+        while not finished:
+            column_string = input("Which column will you drop your token in? ")
+            finished = self.insert_token(player, column_string)
+        # The player has successfully inserted a token
         player.num_tokens -= 1
-        player.my_turn = not player.my_turn
+
+    def check_win_condition(self):
+        #check self.board.slots columns first
+        return False
 
 
 if __name__ == "__main__":
@@ -80,7 +102,7 @@ if __name__ == "__main__":
     if color_letter == "Y":
         player1_color = Color.YELLOW
 
-    player1 = Player(player1_name, player1_color, True)
+    player1 = Player(player1_name, player1_color)
 
     print("Player 1: {}, color {}".format(player1.name, player1.color.value))
 
@@ -92,8 +114,27 @@ if __name__ == "__main__":
     else:
         player2_color = Color.RED
 
-    player2 = Player(player2_name, player2_color, False)
+    player2 = Player(player2_name, player2_color)
     print("Player 2: {}, color {}".format(player2.name, player2.color.value))
+
+    game = Game(player1, player2)
+    win_condition = False
+    player1_turn = True
+    print(game.board)
+
+    while not win_condition and player1.num_tokens > 0: #player 2 can't run out of tokens before player 1
+        if player1_turn:
+            game.play_turn(player1)
+        else:
+            game.play_turn(player2)
+        #switch players
+        player1_turn = not player1_turn
+
+        print(game.board)
+        win_condition = game.check_win_condition()
+
+    #If player1_turn == True, player2 won. If player1_turn == False, player1 won.
+
 
 
 
